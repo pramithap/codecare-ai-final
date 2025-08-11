d491a962f5a26fdf45e2781da4b332c7b4555348
Pramitha Perera <pramithap@gmail.com>
2025-08-12 00:43:59 +1000

    removed clerk authentication

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-  },
 }
 
 module.exports = nextConfig
